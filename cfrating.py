47832ddd083bde34e29898d0b8c4c33404c93a8e
Daksh Rawat <daksh0225@gmail.com>
2019-11-01 22:55:00 +0530

    updated cfrating

--- cfrating.py
+++ cfrating.py
@@ -15,6 +15,7 @@ import sys
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import Select
 
 options=Options()
